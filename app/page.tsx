afdbfd538fae0772b2c95dd6baa9eb28cf7b1e8d
seyitbugraerden <seyitbugraerden@gmail.com>
2024-09-10 03:18:08 +0300

    End of Hero Section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
     <main className="bg-black-100">
       <div>
-        <h1>Hello, Portfolio</h1>
+        <Hero />
       </div>
     </main>
   );
